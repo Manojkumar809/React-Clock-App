cae45c0c1bfc3a0e2fe7d318d9a8465089493534
Manojkumar809 <perisetlam@gmail.com>
2023-08-25 22:22:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const timeid = document.getElementById("time")
+  let myInterval = setInterval(myTime, 1000)
+  const [time, setTime] = useState("12:00:00 AM");
+  const [date, setDate] = useState("01/01/2001")
+  const [day, setDay] = useState("Sunday")
+  const days = {
+    0:'Sunday',
+    1:'Monday',
+    2:'Tuesday',
+    3:'Wednesday',
+    4:'Thursday',
+    5:'Friday',
+    6:'Saturday'
+  }
+  function myTime() {
+    let d = new Date()
+    setTime(d.toLocaleTimeString())
+    setDate(d.toLocaleDateString())
+    setDay(d.getDay())
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 id="date">{date}</h2>
+      <h2 id="time">{time}</h2>
+      <h2 id="day">{days[day]}</h2>
     </div>
   );
 }
